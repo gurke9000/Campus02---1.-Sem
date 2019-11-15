c2fd6f8bb772e793770915cbd7f78266d055a46c
Andreas Ettl <andreas.ettl@edu.campus02.at>
2019-11-15 19:11:09 +0100

    erste Operationen

---
+++ src/airthmOperations.java
@@ -0,0 +1,22 @@
+public class airthmOperations {
+
+    public static void main(String [] args) {
+
+        int a = 7;
+        int b = 3;
+
+        int add = a + b;
+        int sub = a - b;
+        int mult = a * b;
+        int div = a / b;
+        int mod = a % b;
+
+        System.out.println("Wenn A = " + a + " und B = " + b + ", dann gilt:");
+        System.out.println("A + B = " + add);
+        System.out.println("A - B = " + sub);
+        System.out.println("A * B = " + mult);
+        System.out.println("A / B = " + div);
+        System.out.println("A % B = " + mod);
+
+    }
+}
