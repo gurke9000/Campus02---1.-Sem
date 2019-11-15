edd6c6cae1f3c2b9c02ddc9dee2a382bfaab35c2
Andreas Ettl <andreas.ettl@edu.campus02.at>
2019-11-15 18:57:59 +0100

    erste Operationen

---
+++ src/HolaJava.java
@@ -0,0 +1,32 @@
+public class HolaJava {
+//erste Einheit
+//Java ist toll
+
+    public static void main(String[] args) {
+
+        System.out.println("HA, GAAAAAYY!!!");
+
+        int anzahl = 52;
+        char size = 'S';
+
+        byte ki = 55;
+        //lol, das passt
+
+        System.out.println("anzahl:" + anzahl);
+        System.out.println(size);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
