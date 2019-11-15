34ec0efb73c0076468da3deeae95542440b210d3
Andreas Ettl <andreas.ettl@edu.campus02.at>
2019-11-15 19:33:30 +0100

    warum brauch ich da immer ein Kommentar..

---
+++ src/Approx.java
@@ -0,0 +1,9 @@
+public class Approx {
+
+    public static void main(String [] args){
+
+        double ergebnis = 3 + 4 * (1.0/(2*3*4) - 1.0/(4*5*6) + 1.0/(6*7*8));
+
+        System.out.println("ergebnis = " + ergebnis);
+    }
+}
