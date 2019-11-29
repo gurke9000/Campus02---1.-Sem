2f729169eba495f536369e18520dcd818e7724f3
Andreas Ettl <andreas.ettl@edu.campus02.at>
2019-11-29 21:10:17 +0100

    Wiederholung, Generics und andere ganz tolle Dinge, wie zB Packages

---
+++ 29112019/src/org/campus02/calculator/app/DemoApp.java
@@ -0,0 +1,13 @@
+package org.campus02.calculator.app;
+
+import org.campus02.calculator.biz.Calculator;
+
+public class DemoApp {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+
+
+    }
+}
---
+++ 29112019/src/org/campus02/calculator/biz/Calculator.java
@@ -0,0 +1,10 @@
+package org.campus02.calculator.biz;
+
+public class Calculator {
+
+    public static void doSomething()
+    {
+        System.out.println("lol, I do stuffs");
+
+    }
+}
